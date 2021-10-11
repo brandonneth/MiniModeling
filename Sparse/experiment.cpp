f14f5eea13d3da90797c946a642e5a79482afcdb
brandonneth <nethbrandone@gmail.com>
2021-10-11 11:57:26 -0700

    code for the dep rel creation, reading and printing sparse arrays, and the CSC format in its entirety

--- Sparse/experiment.cpp
+++ Sparse/experiment.cpp
@@ -1,33 +1,99 @@
 #include "sparse.hpp"
 
 #include <iostream>
+#include <ctime>
+template <typename T>
+CSR<double> construct_dep_rel_csr_csr(CSR<T> A) {
+    CSR<double> deps(A.num_rows());
+
+    std::size_t N = A.num_rows();
+
+    for(auto i = 0; i < N; i++) {
+        for(auto k = A.rowptr[i]; k < A.rowptr[i+1]; k++) {
+            if(i > A.col[k]) {
+                deps.random_insert(A.col[k], i, 1);
+            }
+        }
+    }
+    return deps;
+}
+
+template <typename T>
+CSC<double> construct_dep_rel_csr_csc(CSR<T> A) {
+    CSC<double> deps(A.num_rows());
+
+    std::size_t N = A.num_rows();
+    for(auto i = 0; i < N; i++) {
+        for(auto k = A.rowptr[i]; k < A.rowptr[i+1]; k++) {
+            if(i > A.col[k]) {
+                deps.ordered_insert(A.col[k], i, 1);
+            }
+        }
+    }
+    return deps;
+}
+
+template <typename T>
+CSR<double> construct_dep_rel_csc_csr(CSC<T> A) {
+    CSR<double> deps(A.num_cols());
+
+    std::size_t N = A.num_cols();
+    for(auto i = 0; i < N; i++) {
+        for(auto k = A.colptr[i]; k < A.colptr[i+1]; k++) {
+            if(i < A.row[k]) {
+                deps.ordered_insert(i, A.row[k], 1);
+            }
+        }
+    }
+    return deps;
+}
+
+template <typename T>
+CSC<double> construct_dep_rel_csc_csc(CSC<T> A) {
+    CSC<double> deps(A.num_cols());
+
+    std::size_t N = A.num_cols();
+    for(auto i = 0; i < N; i++) {
+        for(auto k = A.colptr[i]; k < A.colptr[i+1]; k++) {
+             if(i < A.row[k]) {
+                deps.random_insert(i, A.row[k], 1);
+            }
+        }
+    }
+    return deps;
+}
+
+void compare_execution(std::string filename) {
+    CSR<double> csr = read_csr<double>(filename);
+    CSC<double> csc = read_csc<double>(filename);
+
+    auto rr_start = clock();
+    auto dep_rr = construct_dep_rel_csr_csr(csr);
+    auto rr_end = clock();
+    auto rr_time = rr_end - rr_start;
+
+    auto rc_start = clock();
+    auto dep_rc = construct_dep_rel_csr_csc(csr);
+    auto rc_end = clock();
+    auto rc_time = rc_end - rc_start;
+
+    auto cr_start = clock();
+    auto dep_cr = construct_dep_rel_csc_csr(csc);
+    auto cr_end = clock();
+    auto cr_time = cr_end - cr_start;
+
+    auto cc_start = clock();
+    auto dep_cc = construct_dep_rel_csc_csc(csc);
+    auto cc_end = clock();
+    auto cc_time = cc_end - cc_start;
+
+    std::cout << "CSR CSR: " << rr_time << "\n";
+    std::cout << "CSR CSC: " << rc_time << "\n";
+    std::cout << "CSC CSR: " << cr_time << "\n";
+    std::cout << "CSC CSC: " << cc_time << "\n";
+
+}
+
 int main() {
-
-    CSR<int> a(5);
-
-    a.ordered_insert(0,1,1);
-    a.ordered_insert(1,2,1);
-    a.ordered_insert(3,3,1);
-    a.ordered_insert(3,4,1);
-    a.ordered_insert(4,0,1);
-
-    std::cout << "row ptr: ";
-    for(auto r : a.rowptr) {
-        std::cout << r << " ";
-    }
-    std::cout << "\n";
-
-    std::cout << "col: ";
-    for(auto r : a.col) {
-        std::cout << r << " ";
-    }
-    std::cout << "\n";
-
-    std::cout << "val: ";
-    for(auto r : a.val) {
-        std::cout << r << " ";
-    }
-    std::cout << "\n";
-
-    return 0;
+    compare_execution("smallA.sparse");
 }
--- Sparse/sparse.hpp
+++ Sparse/sparse.hpp
@@ -2,6 +2,8 @@
 #define MINISPARSE_HPP
 
 #include <vector>
+#include <fstream>
+#include <iostream>
 template <typename T>
 struct CSR
 {
@@ -10,16 +12,18 @@ struct CSR
     std::vector<T> val;
 
     CSR(std::size_t N) : rowptr(), col(), val(){
-        rowptr.reserve(N);
+        rowptr.reserve(N+1);
         rowptr.push_back(0);
         rowptr.push_back(0);
     }
     /* data */
 
     std::size_t num_rows() {
+        
         return rowptr.size() - 1;
     }
     void ordered_insert(std::size_t new_row, std::size_t new_col, T new_val) {
+        //std::cout << "Ordered insert " << new_row << ", " << new_col << "\n";
         if(new_row == num_rows() - 1) {
 
         } else {
@@ -33,14 +37,19 @@ struct CSR
     }
 
     void random_insert(std::size_t new_row, std::size_t new_col, T new_val) {
+        //std::cout << "Random insert " << new_row << " , " << new_col << "\n";
+        //std::cout << "num rows: " << num_rows() << "\n";
+        //std::cout << std::flush;
         if(new_row >= num_rows()) { //we haven't inserted something this far along yet
             ordered_insert(new_row, new_col, new_val);
             return;
         }
 
-        auto insert_index = -1;
+        auto insert_index = 0;
         auto start_of_row = rowptr.at(new_row);
         auto end_of_row = rowptr.at(new_row+1);
+
+        //std::cout << "start and end of row: " << start_of_row << ", " << end_of_row << "\n" << std::flush;
         for(auto i = start_of_row; i < end_of_row; i++) {
             if(new_col == col[i]) {
                 val[i] = new_val;
@@ -64,7 +73,7 @@ struct CSR
     }
 
     T random_retrieve(std::size_t r, std::size_t c) {
-        if(r > num_rows) {
+        if(r > num_rows()) {
             return T();
         }
         for(auto i = rowptr[r]; i < rowptr[r+1]; i++) {
@@ -76,15 +85,234 @@ struct CSR
     }
 
     std::size_t row_of(std::size_t c) {
-        
-        return -1;
+        int col_index = -1;
+        for(int i = 0; i < col.size(); i++) {
+            if (col[i] == c) {
+                col_index = i;
+            }
+        }
+        if(col_index == -1) {
+            return -1;
+        }
+
+        // so we know the column we're after is at index col_index of the col array
+        // to get that value's row, we can get the row index that comes after it
+        int row_index = 0;
+        for(int i = 0; i < num_rows(); i++) {
+            if(rowptr[i] <= col_index) {
+                row_index += 1;
+            }
+        }
+        return row_index;
     }
 
     std::size_t col_of(std::size_t r) {
-        return -1;
+       int c = -1;
+       for(int i = rowptr[r]; i < rowptr[r+1]; i++) {
+           c = col[i];
+       }
+       return c;
     }
     
+    friend std::ostream & operator <<(std::ostream & o, CSR<T> csr) {
+        for(int i = 0; i < csr.num_rows(); i++) {
+            for(int k = csr.rowptr[i]; k < csr.rowptr[i+1]; k++) {
+                o << "(" << i << "," << csr.col[k] << "," << csr.val[k] << ")\n";
+            }
+        }
+        return o;
+    }
 };
+
+template <typename T>
+CSR<T> read_csr(const std::string & filename) {
+    std::ifstream file(filename);
+
+    std::size_t cols, rows, vals;
+    file >> rows >> cols >> vals;
+
+    CSR<T> csr(rows);
+    for(int i = 0; i < vals; i++) {
+        std::size_t r, c;
+        T v;
+        file >> r >> c >> v;
+        csr.ordered_insert(r,c,v);
+    }
+    return csr;
+}
+
+
+template <typename T>
+struct CSC
+{
+    std::vector<std::size_t> colptr;
+    std::vector<std::size_t> row;
+    std::vector<T> val;
+
+    CSC(std::size_t N) : colptr(), row(), val(){
+        colptr.reserve(N+1);
+        colptr.push_back(0);
+        colptr.push_back(0);
+    }
+    /* data */
+
+    std::size_t num_cols() {
+        return colptr.size() - 1;
+    }
+    void ordered_insert(std::size_t new_row, std::size_t new_col, T new_val) {
+        
+        if(new_col == num_cols() - 1) {
+
+        } else {
+            for(auto i = num_cols() - 1; i < new_col; i++) {
+                colptr.push_back(row.size());
+            }
+        }
+        row.push_back(new_row);
+        val.push_back(new_val);
+        colptr[num_cols()] += 1;
+    }
+
+    void random_insert(std::size_t new_row, std::size_t new_col, T new_val) {
+        if(new_col >= num_cols()) { //we haven't inserted something this far along yet
+            ordered_insert(new_row, new_col, new_val);
+            return;
+        }
+
+        auto insert_index = 0;
+        auto start_of_col = colptr.at(new_col);
+        auto end_of_col = colptr.at(new_col+1);
+        for(auto i = start_of_col; i < end_of_col; i++) {
+            if(new_row == row[i]) {
+                val[i] = new_val;
+                return;
+            }
+            if(new_row > row[i]) {
+                insert_index = i+1;
+            }
+        }
+
+        auto row_iterator = row.begin() + insert_index;
+        auto val_iterator = val.begin() + insert_index;
+
+        row.insert(row_iterator, new_row);
+        val.insert(val_iterator, new_val);
+
+        //update the col ptr values
+        for(auto i = new_col + 1; i < colptr.size(); i++) {
+            colptr[i] += 1;
+        }
+    }
+
+    T random_retrieve(std::size_t r, std::size_t c) {
+        if(c > num_cols()) {
+            return T();
+        }
+        for(auto i = colptr[c]; i < colptr[c+1]; i++) {
+            if(row[i] == r) {
+                return val[i];
+            }
+        }
+        return 0;
+    }
+
+    std::size_t row_of(std::size_t c) {
+        int r = -1;
+        for(int i = colptr[c]; i < colptr[c+1]; i++) {
+            r = row[i];
+        }
+        return r;
+        
+    }
+
+    std::size_t col_of(std::size_t r) {
+       int row_index = -1;
+       for(int i = 0; i < row.size(); i++) {
+           if (row[i] == r){
+               row_index = i;
+           }
+       }
+       if(row_index == -1) {
+           return -1;
+       }
+
+       int col_index = -1;
+       for(int i = 0; i < num_cols(); i++) {
+           if(colptr[i] <= row_index) {
+               col_index += 1;
+           }
+       }
+       return row_index;
+    }
+    
+    friend std::ostream & operator <<(std::ostream & o, CSC<T> csc) {
+        for(int i = 0; i < csc.num_cols(); i++) {
+            for(int k = csc.colptr[i]; k < csc.colptr[i+1]; k++) {
+                o << "(" << csc.row[k] << "," << i << "," << csc.val[k] << ")\n";
+            }
+        }
+        return o;
+    }
+}; //CSC<T>
+
+template <typename T>
+CSC<T> read_csc(const std::string & filename) {
+    std::ifstream file(filename);
+
+    std::size_t cols, rows, vals;
+    file >> rows >> cols >> vals;
+
+    CSC<T> csc(cols);
+    for(int i = 0; i < vals; i++) {
+        std::size_t r, c;
+        T v;
+        file >> r >> c >> v;
+        csc.random_insert(r,c,v);
+    }
+    return csc;
+}
+
+template <typename T>
+void print_csr(CSR<T> csr) {
+    std::cout << "rowptr: ";
+    for(auto i = 0; i < csr.rowptr.size(); i++) {
+        std::cout << csr.rowptr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "   col: ";
+    for(auto i = 0; i < csr.col.size(); i++) {
+        std::cout << csr.col[i] << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "   val: ";
+    for(auto i = 0; i < csr.val.size(); i++) {
+        std::cout << csr.val[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+template <typename T>
+void print_csc(CSC<T> csc) {
+    std::cout << "colptr: ";
+    for(auto i = 0; i < csc.colptr.size(); i++) {
+        std::cout << csc.colptr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "   row: ";
+    for(auto i = 0; i < csc.row.size(); i++) {
+        std::cout << csc.row[i] << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "   val: ";
+    for(auto i = 0; i < csc.val.size(); i++) {
+        std::cout << csc.val[i] << " ";
+    }
+    std::cout << std::endl;
+}
 
 
 
